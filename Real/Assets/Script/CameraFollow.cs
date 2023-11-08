9fb5d186fa835e33455e2853d94d8c1a2946a3b2
Pomz90 <149971586+Pomz90@users.noreply.github.com>
2023-11-08 21:28:50 +0700

    camera bound

--- Real/Assets/Script/CameraFollow.cs
+++ Real/Assets/Script/CameraFollow.cs
@@ -1,17 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-	public float FollowSpeed = 2f;
-	public float yOffset = 1f;
-	public Transform target;
+    public Transform target;
+    public Vector3 offset;
+    [Range(1, 10)]
+    public float smoothFactor;
+    [HideInInspector]
+    public Vector3 minValues, maxValue;
 
-	// Update is called once per frame
-	void Update()
-	{
-		Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset, -10f);
-		transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
-	}
+
+    //Editors Fields
+    [HideInInspector]
+    public bool setupComplete = false;
+    public enum SetupState { None, Step1, Step2 }
+    [HideInInspector]
+    public SetupState ss = SetupState.None;
+
+    private void FixedUpdate()
+    {
+        Follow();
+    }
+
+    void Follow()
+    {
+        Vector3 targetPosition = target.position + offset;
+        //Verify if the targetPosition is out of bound or not
+        //Limit it to the min and max values
+        Vector3 boundPosition = new Vector3(
+            Mathf.Clamp(targetPosition.x, minValues.x, maxValue.x),
+            Mathf.Clamp(targetPosition.y, minValues.y, maxValue.y),
+            Mathf.Clamp(targetPosition.z, minValues.z, maxValue.z));
+
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, boundPosition, smoothFactor * Time.fixedDeltaTime);
+        transform.position = smoothPosition;
+    }
+
+    public void ResetValues()
+    {
+        setupComplete = false;
+        minValues = Vector3.zero;
+        maxValue = Vector3.zero;
+    }
 }
